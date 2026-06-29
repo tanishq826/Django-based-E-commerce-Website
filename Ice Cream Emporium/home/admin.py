92586533f1c7b9a163f4bcc032d7f8993afb2b07
Tanishq Saini <149817870+tanishq826@users.noreply.github.com>
2026-06-29 21:55:47 +0530

    Delete Ice Cream Emporium directory

--- Ice Cream Emporium/home/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from home.models import Contact,Products,Cart,Category
-
-# Register your models here.
-admin.site.register(Contact)
-admin.site.register(Products)
-admin.site.register(Cart)
-admin.site.register(Category)
